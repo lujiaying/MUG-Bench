5fc478c42911c6499aa6f88c7bde9ed0814edf9a
jiaying.lu <lujiaying93@foxmail.com>
2022-12-16 22:25:14 -0500

    current version of plots

--- visualization/plot_3d_scatter.py
+++ visualization/plot_3d_scatter.py
@@ -36,16 +36,17 @@ if __name__ == '__main__':
     marker_size=80
     alpha=0.50
     ax.scatter(x_pokemon, y_pokemon, z_pokemon, c="darkorange", s=marker_size, alpha=alpha)
+    ax.scatter(x_hearthstone, y_hearthstone, z_hearthstone, c="royalblue", s=marker_size, alpha=alpha)
     ax.scatter(x_league, y_league, z_league, c="firebrick", s=marker_size, alpha=alpha)
     ax.scatter(x_csgo, y_csgo, z_csgo, c="forestgreen", s=marker_size, alpha=alpha)
-    ax.scatter(x_hearthstone, y_hearthstone, z_hearthstone, c="royalblue", s=marker_size, alpha=alpha)
 
     fontsize = 26
-    ax.set_xlabel("Red Channel", fontsize=fontsize, labelpad=9)
-    ax.set_ylabel("Green Channel", fontsize=fontsize, labelpad=9)
-    ax.set_zlabel("Blue Channel", fontsize=fontsize, labelpad=9)
+    ax.set_xlabel("R", fontsize=fontsize, labelpad=7)
+    ax.set_ylabel("G", fontsize=fontsize, labelpad=7)
+    ax.set_zlabel("B", fontsize=fontsize, labelpad=6)
 
-    ax.tick_params(axis='both', which='major', labelsize=16)
-    legend = ax.legend(["pkm", "lol", "csg", "hs"], fontsize=38, loc='upper center', ncol=2,
-                       bbox_to_anchor=(0.5, 1.0), shadow=False)
+    ax.tick_params(axis='both', which='major', labelsize=20)
+    legend = ax.legend(["pkm", "hs", "lol", "csg"], fontsize=38, loc='upper center', ncol=1,
+                       bbox_to_anchor=(0.2, 0.90), markerscale=2., shadow=False,
+                       labelspacing=.3, handletextpad=0.1)
     ax.figure.savefig('MeanRGBScatter3D.pdf', bbox_inches='tight')
